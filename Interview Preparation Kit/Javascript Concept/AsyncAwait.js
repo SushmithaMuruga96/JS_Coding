71a08b7f4c3d73514d237e6fa14f4d6321eda516
SushmithaMuruga96 <s30496usi@gmail.com>
2026-01-24 10:50:41 +0400

    error handling with fetch

--- Interview Preparation Kit/Javascript Concept/AsyncAwait.js
+++ Interview Preparation Kit/Javascript Concept/AsyncAwait.js
@@ -1,26 +1,26 @@
-const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise 1 resolved");
-  }, 5000);
-});
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Promise 1 resolved");
+//   }, 5000);
+// });
 
-const p2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise 2 resolved");
-  }, 10000);
-});
+// const p2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Promise 2 resolved");
+//   }, 10000);
+// });
 
-async function getData() {
-  console.log("Fetching data..."); //1
-  const res1 = await p1; // waits here until p1 is resolved
-  console.log(res1); //2
+// async function getData() {
+//   console.log("Fetching data..."); //1
+//   const res1 = await p1; // waits here until p1 is resolved
+//   console.log(res1); //2
 
-  const res2 = await p2; // waits here until p2 is resolved
-  console.log(res2); //3
-  console.log("All data fetched"); //4
-}
+//   const res2 = await p2; // waits here until p2 is resolved
+//   console.log(res2); //3
+//   console.log("All data fetched"); //4
+// }
 
-getData();
+// getData();
 
 // async function getData() {
 //   //   return "Namaste";
@@ -49,3 +49,16 @@ getData();
 //await suspends the code and continues to execute from there when the promise is settled
 //thats developer uses async-await than normal promise chaining
 //for better understanding of code flow
+
+// const API_URL = "https://api.github.com/users/SushmithaMuruga96";
+const API_URL = "https://someinvalidurl.com/data"; // to test error handling
+async function getData() {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log("Error occurred:", err);
+  }
+}
+getData();
